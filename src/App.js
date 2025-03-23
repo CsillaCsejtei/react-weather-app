1545abf1289fd8c8ced36646a623cf67a667c187
CsillaCsejtei <csillacsejtei@gmail.com>
2025-03-23 19:07:08 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Csilla Csejtei and is{" "}
-        <a
-          href="https://github.com/CsillaCsejtei/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by Csilla Csejtei and is{" "}
+          <a
+            href="https://github.com/CsillaCsejtei/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
