9276124e5a168bd007507e60749d070a7942c357
CsillaCsejtei <csillacsejtei@gmail.com>
2025-03-23 17:11:46 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Csilla Csejtei and is{" "}
+        <a
+          href="https://github.com/CsillaCsejtei/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
