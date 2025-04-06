c2f9458dbe8df1b36bd590bf8aa2e11ea5db3bf3
CsillaCsejtei <csillacsejtei@gmail.com>
2025-04-06 22:37:24 +0100

    Added unit switching (2)

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,13 +15,13 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div>
+          <div className="icon-temperature">
+            <div className="icon">
             <WeatherIcon code={props.data.icon}/>
-
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
+            </div>
+            <div>
+            <WeatherTemperature celsius={props.data.temperature}/>
+           </div>
           </div>
         </div>
         <div className="col-6">
